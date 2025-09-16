12d895d5e2eedfed1605895e6584b213d72eb3c7
Dylan-Austin <dylanaustinrhys@gmail.com>
2025-09-16 13:53:46 +0100

    changed names of files

--- Quant Research/main.py
+++ Quant Research/main.py
@@ -1,1 +1,20 @@
-print("Hello Dylan")
+from data_test import get_processed_stock_data
+import pandas as pd
+from RandomForest import RandomForestResearchModel
+from xgboost import XGB
+
+def process_data():
+    column_to_predict = "Close"
+
+    stock_data = get_processed_stock_data()
+
+    XTrain = stock_data.drop(columns=[column_to_predict])
+    YTrain = stock_data[column_to_predict]
+
+    return XTrain, YTrain
+
+XTrain, YTrain = process_data()
+
+model = RandomForestResearchModel()
+
+model.fit(XTrain, YTrain)
